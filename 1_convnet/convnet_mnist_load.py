c0b56592dc4d2eb31ce03f6ef52a0db7292ea506
JackyLiu <jjkka132@gmail.com>
2017-07-28 16:14:22 +0000

    add comment

--- 1_convnet/convnet_mnist_load.py
+++ 1_convnet/convnet_mnist_load.py
@@ -11,6 +11,9 @@ import tensorflow as tf
 import numpy as np
 import scipy.ndimage
 
+import sys
+
+## Define network
 network = input_data(shape=[None, 28, 28, 1], name='input')
 network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
 network = max_pool_2d(network, 2)
@@ -26,29 +29,26 @@ network = fully_connected(network, 10, activation='softmax')
 network = regression(network, optimizer='adam', learning_rate=0.01,
                      loss='categorical_crossentropy', name='target')
 
+## Build model
 model = tflearn.DNN(network, tensorboard_verbose=0, tensorboard_dir='/notebooks/logs/tflearn_logs/')
 
-# Load a model
+## Load a model from file
 model.load('my_model.tflearn')
 
-import sys
-
-
+## Load testing image from command line argument
+##  example:
+##          python3 convnet_mnist_load.py ../test.png
 data = scipy.ndimage.imread(sys.argv[1], flatten=True)
-#data = scipy.ndimage.imread("../testing5.png", flatten=True)
 data = np.vectorize(lambda x: 255 - x)(np.reshape(data, (1,28,28,1)))
 data = data/255.  # image value is 0.0 ~ 1.0
 
-#predictions = np.array(model.predict(testX)).argmax(axis=1)
-#actual = testY.argmax(axis=1)
-#test_accuracy = np.mean(predictions == actual, axis=0)
-#print("Test accuracy: ", test_accuracy)
 
-#predictions = np.array(model.predict(data)).argmax(axis=1)
-#print(predictions)       
 result = model.predict(data)
 print(result)
 print('Predicted number = ' + str(np.argmax(result, 1)[0]))
 
-
-
+## Predict on mnist and print accuracy
+#predictions = np.array(model.predict(testX)).argmax(axis=1)
+#actual = testY.argmax(axis=1)
+#test_accuracy = np.mean(predictions == actual, axis=0)
+#print("Test accuracy: ", test_accuracy)
